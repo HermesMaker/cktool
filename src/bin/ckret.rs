d35aa422b074bfe3bd5e173a14dac026ca107f97
HermesMaker <xkbsoul@proton.me>
2026-01-19 17:07:17 +0000

    ckret: add feature continue download by checking exist files.

--- src/bin/ckret.rs
+++ src/bin/ckret.rs
@@ -7,11 +7,12 @@ use clap::Parser;
 use colored::Colorize;
 use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::StatusCode;
+use reqwest::{StatusCode, header::RANGE};
 use std::{cmp::min, path::Path, time::Duration};
 use tokio::{
     fs,
     io::{AsyncWriteExt, BufWriter},
+    time::sleep,
 };
 
 #[derive(Parser)]
@@ -35,7 +36,7 @@ struct Args {
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let mut urls = read_file(&args.file).await?;
-    let retry = args.retry.unwrap_or(100);
+    let retry = args.retry.unwrap_or(100); // use for base value.
     let out = match args.out {
         Some(_as) => _as,
         None => ".".to_string(),
@@ -56,6 +57,9 @@ async fn main() -> anyhow::Result<()> {
 
     let url_len = urls.len();
     for i in 0..url_len {
+        if urls[i].is_empty() {
+            continue;
+        }
         if let Some(first_char) = urls[i].chars().next()
             && first_char == '#'
         {
@@ -84,40 +88,74 @@ pub async fn read_file(file: &str) -> anyhow::Result<Vec<String>> {
 /// this func use to download each url.
 pub async fn download(url: &str, retry: u32, out: &str, index: u64) -> anyhow::Result<()> {
     if let Some(file_name) = url.split("/").last() {
-        let client = request::new()?;
+        let path_to_file = format!("{}/{}", out, file_name);
+        let mut retry_request = retry;
+        'request: loop {
+            // check file exist to get fiel size to continue download by set range header.
+            // if not found download from zero
+            let (sender, file_size) = if let Ok(result) = Path::new(&path_to_file).try_exists()
+                && result
+            {
+                let file_size = tokio::fs::metadata(&path_to_file)
+                    .await
+                    .context("cannot get file size")?
+                    .len();
+                let create_sender = request::new()?
+                    .get(url)
+                    .header(RANGE, format!("bytes={}-", file_size));
+                (create_sender, Some(file_size))
+            } else {
+                (request::new()?.get(url), None)
+            };
 
-        let file = tokio::fs::File::create(format!("{}/{}", out, file_name)).await?;
-        let mut file = BufWriter::new(file);
-        let mut retry = retry;
-        loop {
-            if let Ok(response) = client.get(url).send().await {
+            // create or open file follow by file_size.
+            let file = if file_size.is_some() {
+                tokio::fs::OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open(&path_to_file)
+                    .await?
+            } else {
+                tokio::fs::File::create(&path_to_file).await?
+            };
+            let mut file = BufWriter::new(file);
+
+            if let Ok(response) = sender.send().await {
                 let total_size = match response.content_length().context("Cannot get total size") {
                     Ok(v) => v,
                     Err(_) => {
                         eprintln!("Failed receive file size");
-                        if retry > 0 {
-                            retry -= 1;
+                        if retry_request > 0 {
+                            retry_request -= 1;
                             continue;
                         }
                         return Err(anyhow!("Failed download"));
                     }
                 };
 
+                // this download was clompleted.
+                if 190 == response.status() || 416 == response.status() {
+                    println!("{} {}", "success".green().bold(), file_name.blue());
+                    break 'request;
+                }
                 // prevent too many requests
                 if StatusCode::TOO_MANY_REQUESTS == response.status() {
                     tokio::time::sleep(Duration::from_secs(TOO_MANY_REQUESTS_DELAY_SEC)).await;
                     continue;
                 }
                 // prevent bad gateway: wait 2 secs and re-download
-                if StatusCode::OK != response.status() {
-                    if retry == 0 {
+                if StatusCode::OK != response.status()
+                    && StatusCode::PARTIAL_CONTENT != response.status()
+                {
+                    if retry_request == 0 {
                         return Err(anyhow!("Failed download"));
                     }
-                    retry -= 1;
+                    retry_request -= 1;
                     tokio::time::sleep(Duration::from_secs(ERROR_REQUEST_DELAY_SEC)).await;
                     continue;
                 }
-                let p = create_progress_bar(total_size);
+                let mut downloaded: u64 = file_size.unwrap_or(0);
+                let p = create_progress_bar(total_size + downloaded);
                 p.set_message(format!(
                     "[{}] {} {}",
                     index,
@@ -126,14 +164,14 @@ pub async fn download(url: &str, retry: u32, out: &str, index: u64) -> anyhow::R
                 ));
 
                 let mut stream = response.bytes_stream();
-                let mut downloaded: u64 = 0;
 
-                while let Some(item) = stream.next().await {
+                #[allow(unused)]
+                'receive_item: while let Some(item) = stream.next().await {
                     match item {
                         Ok(item) => {
-                            if (file.write_all(&item).await).is_err() {
-                                eprintln!("Failed writes bytes to file");
-                                return Err(anyhow!("Failed download"));
+                            let mut retry_write = 10;
+                            while (file.write_all(&item).await).is_err() && retry_write > 0 {
+                                retry_write -= 1;
                             }
 
                             let new = min(downloaded + (item.len() as u64), total_size);
@@ -141,7 +179,15 @@ pub async fn download(url: &str, retry: u32, out: &str, index: u64) -> anyhow::R
                             p.set_position(new);
                         }
                         Err(_) => {
-                            continue;
+                            // failed while downloading.
+                            p.set_message(format!(
+                                "[{}] {} {}",
+                                index,
+                                "Reconnect".yellow().bold(),
+                                file_name
+                            ));
+                            sleep(Duration::from_secs(1)).await;
+                            continue 'request;
                         }
                     }
                 }
@@ -153,8 +199,8 @@ pub async fn download(url: &str, retry: u32, out: &str, index: u64) -> anyhow::R
                     "success".green().bold(),
                     file_name
                 ));
-            } else if retry > 0 {
-                retry -= 1;
+            } else if retry_request > 0 {
+                retry_request -= 1;
                 tokio::time::sleep(Duration::from_secs(ERROR_REQUEST_DELAY_SEC)).await;
                 continue;
             } else {
